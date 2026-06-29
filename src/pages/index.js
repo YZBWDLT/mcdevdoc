603b04c069bb8c6ababf9bb51c197b111d1aa058
yzbwdlt <yzbwdlt@163.com>
2026-06-29 20:17:37 +0800

    [新增] 26.06.29 文档更新 - 更新了文档为 OreUI 风格。感谢萝卜！

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
@@ -9,22 +10,35 @@ import HomepageFeatures from '../components/HomepageFeatures';
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <header className={styles.heroBanner}>
             <div className="container">
-                <Heading as="h1" className="hero__title">
+                <Heading as="h1" className={styles.heroTitle}>
                     {siteConfig.title}
                 </Heading>
-                <p className="hero__subtitle">
+                <p className={styles.heroSubtitle}>
                     {siteConfig.tagline}
                 </p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="btn green_btn large_btn"
+                        to="/docs/tutorials/a1_commands/b0_introduction">
+                        开始学习
+                    </Link>
+                    <Link
+                        className="btn normal_btn large_btn"
+                        to="/blog">
+                        个人博客
+                    </Link>
+                </div>
             </div>
         </header>
     );
 }
 
 export default function Home() {
+    const {siteConfig} = useDocusaurusContext();
     return (
-        <Layout title={`主页`} description="">
+        <Layout title={`主页`} description={siteConfig.tagline}>
             <HomepageHeader />
             <main>
                 <HomepageFeatures />
